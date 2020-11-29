d5732cbe6c10f6cdd410d27fb4fdac593dea346d
Hamza Alsharif <31031372+hsfalsharif@users.noreply.github.com>
2020-11-29 16:46:14 +0300

    Update divider_wrapper.v

--- divider_wrapper.v
+++ divider_wrapper.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
-module divider_wrapper(input clk, sclr, [19:0] dividend, [11:0] divisor,
+module divider_wrapper(input clk, en, sclr, [19:0] dividend, [11:0] divisor,
 	 output wire ready, wire [19:0] quotient);
 	 
 	 assign ready = (quotient == 0) ? 0 : 1;
 	 
 		divider d (
 			.clk(clk), // input clk
-			.ce(clk), // input ce
+			.ce(en), // input ce
 			.sclr(sclr), // input sclr
 			.dividend(dividend), // input [19 : 0] dividend
 			.divisor(divisor), // input [11 : 0] divisor
