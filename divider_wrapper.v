ee86bb212d2b21dbc3f1605b8e672bd3cacb3a2d
Hamza Alsharif <31031372+hsfalsharif@users.noreply.github.com>
2020-11-30 01:14:38 +0300

    Update divider_wrapper.v

--- divider_wrapper.v
+++ divider_wrapper.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
-module divider_wrapper(input clk, ce, en, sclr, [19:0] dividend, [11:0] divisor,
+module divider_wrapper(input clk, ce, sclr, en, [19:0] dividend, [11:0] divisor,
 	 output wire ready, wire [19:0] quotient);
 	 
-	 assign ready = (quotient == 0) ? 0 : 1;
+	 assign ready = (quotient == 12'b0) ? 0 : 1;
 	 assign enable = (ce && en); // ce is enabled when we need to divide while en is enabled when the cluster mean is activated for the whole algorithm
 		divider d (
 			.clk(clk), // input clk
